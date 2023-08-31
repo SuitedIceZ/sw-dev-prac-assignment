ad62d8ef59dc3dab9db56189d8e744560c05f145
SuitedIceZ <okshopm575@gmail.com>
2023-08-31 15:03:44 +0700

    assignment04_fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 import Banner from '@/components/Banner'
 import ProductCard from '@/components/ProductCard'
 
@@ -7,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Banner/>
-      <div style={{margin:'20px',display:'flex',flexDirection:"row",alignContent:"space-around",justifyContent:"space-around",flexWrap:"wrap"}}>
+      <div className="m-5 flex flex-row content-around justify-around flex-wrap">
         <ProductCard hospitalName='Chulalongkorn Hospital' imgSrc='/img/chula.jpg'/>
         <ProductCard hospitalName='Rajavithi Hospital' imgSrc='/img/rajavithi.jpg'/>
         <ProductCard hospitalName='Thammasat University Hospital' imgSrc='/img/thammasat.jpg'/>

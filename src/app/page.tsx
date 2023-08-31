97f5e79057d59af421269e0b1943c3252010f01b
SuitedIceZ <okshopm575@gmail.com>
2023-08-31 09:39:35 +0700

    Feat: add props for ProductCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,10 @@ export default function Home() {
   return (
     <main>
       <Banner/>
-      <div style={{margin:'20px'}}>
-        <ProductCard/>
+      <div style={{margin:'20px',display:'flex',flexDirection:"row",alignContent:"space-around",justifyContent:"space-around",flexWrap:"wrap"}}>
+        <ProductCard hospitalName='Chulalongkorn Hospital' imgSrc='/img/chula.jpg'/>
+        <ProductCard hospitalName='Rajavithi Hospital' imgSrc='/img/rajavithi.jpg'/>
+        <ProductCard hospitalName='Thammasat University Hospital' imgSrc='/img/thammasat.jpg'/>
       </div>
     </main>
   )
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,16 @@
 import style from './productcard.module.css'
 import Image from 'next/image'
 
-export default function ProductCard () {
+export default function ProductCard ( { hospitalName, imgSrc } : { hospitalName:string, imgSrc:string } ) {
     return (
         <div className={style.card}>
             <div className={style.cardimg}>
-            <Image src={'/img/vaccine.jpg'}
+            <Image src={imgSrc}
             alt='Vaccine Picture' 
             fill={true}
             objectFit='cover'/>
             </div>
-            <div className={style.cardtext}>วัคซีน (Vaccine) คือ สารชนิดหนึ่งที่ฉีดเข้าสู่ร่างกาย 
-            เพื่อกระตุ้นให้ร่างกายสร้างภูมิคุ้มกันโรค ในวัคซีนมีเชื้อโรคที่ไม่ทำให้เกิดโรค</div>
+            <div className={style.cardtext}>{hospitalName}</div>
         </div>
     )
 }

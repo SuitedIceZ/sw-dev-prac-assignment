1f86e285b40962adbb3779375b3a8158bcc81564
SuitedIceZ <okshopm575@gmail.com>
2023-10-29 19:09:16 +0700

    Assignment 10

---
+++ interfaces.ts
@@ -0,0 +1,10 @@
+import { Dayjs } from "dayjs"
+
+export interface BookingItem {
+    // hospitalId: string
+    name: string
+    surname: string
+    citizenId: string
+    hospitalName: string
+    bookingDate: string
+}
